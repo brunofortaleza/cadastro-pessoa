b66b190ef71090adf1ad1c243109ffddc31e9b87
Bruno <brunosilvapalmas@gmail.com>
2020-03-23 10:41:28 -0300

    Adicionado o tamanho da lista de pessoas cadastradas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,23 @@ import { Component } from '@angular/core';
 export class AppComponent {
   pessoa = null;
   lista_pessoas = []
+  total = null;
 
   adicionar(){
     this.lista_pessoas.push(this.pessoa)
     this.pessoa = null;
+
+    this.totalizador();
   }
 
   excluir(){
     this.lista_pessoas.pop();
+
+    this.totalizador();
+  }
+
+  totalizador(){
+    this.total = this.lista_pessoas.length;
   }
 
 }
